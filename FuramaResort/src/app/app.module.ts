38f85639b8f850cc8219580f38dcb7229a6c939b
phuongnhi1604 <phuongnhi1641@gmail.com>
2022-08-23 20:06:36 +0700

    up casestudy

--- FuramaResort/src/app/app.module.ts
+++ FuramaResort/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FacilitiesListComponent } from './facilities-list/facilities-list.component';
+import { FacilitiesEditComponent } from './facilities-edit/facilities-edit.component';
+import { FacilitiesCreateComponent } from './facilities-create/facilities-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FacilitiesListComponent,
+    FacilitiesEditComponent,
+    FacilitiesCreateComponent
   ],
   imports: [
     BrowserModule
