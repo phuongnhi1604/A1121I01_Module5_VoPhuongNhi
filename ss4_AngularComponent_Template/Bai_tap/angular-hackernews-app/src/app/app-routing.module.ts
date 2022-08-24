6fc0ea1286a6197c752a960ae942b7032d33006d
phuongnhi1604 <phuongnhi1641@gmail.com>
2022-08-24 20:33:16 +0700

    up bai tap ss4

---
+++ ss4_AngularComponent_Template/Bai_tap/angular-hackernews-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {ArticleComponent} from './article/article.component';
+
+
+const routes: Routes = [
+  {path: 'article', component: ArticleComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
